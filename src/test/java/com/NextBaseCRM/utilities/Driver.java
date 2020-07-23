a9d20ceac5110ccd47909b1c7c47188943fbed46
Winta Tedros <wtedros@gmail.com>
2020-07-23 19:36:00 -0400

    first commit

--- src/test/java/com/NextBaseCRM/utilities/Driver.java
+++ src/test/java/com/NextBaseCRM/utilities/Driver.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class git Driver {
+public class Driver {
 
     private Driver(){
 
